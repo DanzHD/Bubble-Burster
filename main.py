62f17717a564b8119c91932dcf94d9b74fad60ec
daniel.d.cow@gmail.com <daniel.d.cow@gmail.com>
2024-02-25 17:18:51 +1100

    Added game over screen

--- main.py
+++ main.py
@@ -4,9 +4,10 @@ from Config import Config
 from Bubble import Bubble
 from Shooter import Shooter
 
+screen = pygame.display.set_mode((Config.SCREEN_WIDTH, Config.SCREEN_HEIGHT))
+
 
 def main():
-    screen = pygame.display.set_mode((Config.SCREEN_WIDTH, Config.SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     running = True
 
@@ -35,7 +36,9 @@ def main():
             else:
                 player_2_shooter.projectiles.remove(projectile)
             if projectile.collide_bubble(player_1_bubble):
-                print("Game over")
+                game_over()
+                running = False
+                break
 
         player_1_bubble.handle_movement(key)
         player_2_shooter.handle_controls(key)
@@ -44,6 +47,31 @@ def main():
         clock.tick(Config.FPS)
 
     pygame.quit()
+
+
+def game_over():
+    time_lasted = str(pygame.time.get_ticks() / 1000)
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    break
+        key = pygame.key.get_pressed()
+        if key[pygame.K_ESCAPE]:
+            break
+
+
+        screen.fill((0, 0, 0))
+        font = pygame.font.SysFont('arial', 40)
+        title = font.render('GameOver', True, (255, 255, 255))
+        start_button = font.render('Bubble lasted: ' + time_lasted + " Seconds", True, (255, 255, 255))
+        screen.blit(title, (
+            Config.SCREEN_WIDTH / 2 - title.get_width() / 2, Config.SCREEN_HEIGHT / 2 - title.get_height() / 2))
+        screen.blit(start_button,
+                    (Config.SCREEN_WIDTH / 2 - start_button.get_width() / 2,
+                     Config.SCREEN_HEIGHT / 2 + start_button.get_height() / 2))
+        pygame.display.update()
+        pygame.display.flip()
 
 
 if __name__ == "__main__":
